657e958722ab2325b49e10dbd9d668e25cdfbbda
itcontent <itcontent@2f6c41ca-324e-0410-a569-9b2e432399e1>
2009-03-19 04:10:08 +0000

    msn-logic Add implements of DisplayPicture related method.

--- trunk/msn-logic/src/com/microblog/ws/impl/ChangeDisplayPicServiceImpl.java
+++ trunk/msn-logic/src/com/microblog/ws/impl/ChangeDisplayPicServiceImpl.java
@@ -1,21 +1,70 @@
 package com.microblog.ws.impl;
 
+import java.io.File;
+import java.io.FileInputStream;
+
+import javax.xml.namespace.QName;
+
+import org.apache.axis2.addressing.EndpointReference;
+import org.apache.axis2.client.Options;
+import org.apache.axis2.rpc.client.RPCServiceClient;
+
 import com.microblog.ws.intf.IChangeDisplayPicService;
 
 public class ChangeDisplayPicServiceImpl implements IChangeDisplayPicService {
 
-	@Override
-	public boolean changeMessengerDisplayPic(String passport, String passcode,
-			String account, byte[] pic) {
-		// TODO Auto-generated method stub
-		return false;
+	private EndpointReference targetEPR;
+	private RPCServiceClient serviceClient;
+
+	private String ns = "http://changeDisplayPic.webservice.msn.microblog.com";
+
+	public ChangeDisplayPicServiceImpl(String url) throws Exception {
+		targetEPR = new EndpointReference(url);
+		serviceClient = new RPCServiceClient();
+		Options options = serviceClient.getOptions();
+		options.setTo(targetEPR);
 	}
 
+	@SuppressWarnings("unchecked")
+	@Override
+	public boolean changeMessengerDisplayPic(String passport, String passcode,
+			String account, String picPath) throws Exception {
+		File file = new File(picPath);
+		if (!file.exists())
+			return false;
+		long len = file.length();
+		if (((float) len / 1024f) > 30)
+			return false;
+		FileInputStream fin = new FileInputStream(file);
+		byte[] b = new byte[(int) len];
+		fin.read(b, 0, b.length);
+		fin.close();
+		Object[] opAddEntryArgs = new Object[] { passport, passcode, account, b };
+		Class[] classes = new Class[] { Boolean.class };
+		QName opAddEntry = new QName(ns, "changeMessengerDisplayPic");
+		return (Boolean) (serviceClient.invokeBlocking(opAddEntry,
+				opAddEntryArgs, classes)[0]);
+	}
+
+	@SuppressWarnings("unchecked")
 	@Override
 	public boolean changeServiceDisplayPic(String passport, String passcode,
-			byte[] pic) {
-		// TODO Auto-generated method stub
-		return false;
+			String picPath) throws Exception {
+		File file = new File(picPath);
+		if (!file.exists())
+			return false;
+		long len = file.length();
+		if (((float) len / 1024f) > 30)
+			return false;
+		FileInputStream fin = new FileInputStream(file);
+		byte[] b = new byte[(int) len];
+		fin.read(b, 0, b.length);
+		fin.close();
+		Object[] opAddEntryArgs = new Object[] { passport, passcode, b };
+		Class[] classes = new Class[] { Boolean.class };
+		QName opAddEntry = new QName(ns, "changeServiceDisplayPic");
+		return (Boolean) (serviceClient.invokeBlocking(opAddEntry,
+				opAddEntryArgs, classes)[0]);
 	}
 
 }
--- trunk/msn-logic/src/com/microblog/ws/intf/IChangeDisplayPicService.java
+++ trunk/msn-logic/src/com/microblog/ws/intf/IChangeDisplayPicService.java
@@ -3,9 +3,9 @@ package com.microblog.ws.intf;
 public interface IChangeDisplayPicService {
 
 	public boolean changeMessengerDisplayPic(String passport, String passcode,
-			String account, byte[] pic);
+			String account, String picPath) throws Exception;
 
 	public boolean changeServiceDisplayPic(String passport, String passcode,
-			byte[] pic);
+			String picPath) throws Exception;
 
 }
