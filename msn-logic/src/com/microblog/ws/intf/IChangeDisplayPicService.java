29e88920a857e1f2c9c21458bb5d0c4c76baf8f6
itcontent <itcontent@2f6c41ca-324e-0410-a569-9b2e432399e1>
2009-03-23 09:17:13 +0000

    msn-logic Add ChangeDisplayPic with base64 string parameter

--- msn-logic/src/com/microblog/ws/intf/IChangeDisplayPicService.java
+++ msn-logic/src/com/microblog/ws/intf/IChangeDisplayPicService.java
@@ -5,7 +5,13 @@ public interface IChangeDisplayPicService {
 	public boolean changeMessengerDisplayPic(String passport, String passcode,
 			String account, String picPath) throws Exception;
 
+	public boolean changeMessengerDisplayPicBase64(String passport,
+			String passcode, String account, String picPath) throws Exception;
+
 	public boolean changeServiceDisplayPic(String passport, String passcode,
 			String picPath) throws Exception;
+
+	public boolean changeServiceDisplayPicBase64(String passport,
+			String passcode, String picPath) throws Exception;
 
 }
