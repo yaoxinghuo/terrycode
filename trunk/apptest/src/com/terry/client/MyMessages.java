995f828f0db63dca1e128eceadbb748157164be7
yaoxinghuo <yaoxinghuo@2f6c41ca-324e-0410-a569-9b2e432399e1>
2009-04-10 17:37:02 +0000

    apptest add i18n test.

---
+++ trunk/apptest/src/com/terry/client/MyMessages.java
@@ -0,0 +1,11 @@
+package com.terry.client;
+
+import com.google.gwt.i18n.client.Messages;
+
+/**
+ * @author Terry E-mail: yaoxinghuo at 126 dot com
+ * @version create：Apr 11, 2009 12:12:20 AM
+ */
+public interface MyMessages extends Messages {
+	String reply(String input);
+}
--- trunk/apptest/src/com/terry/servlet/TestServlet.java
+++ trunk/apptest/src/com/terry/servlet/TestServlet.java
@@ -1,7 +1,6 @@
 package com.terry.servlet;
 
 import java.io.IOException;
-import java.util.Date;
 import java.util.List;
 
 import javax.jdo.PersistenceManager;
