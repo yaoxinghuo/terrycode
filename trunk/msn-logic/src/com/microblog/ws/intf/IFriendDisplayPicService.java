df7c4f830e9bf41ae194d1c6db3d0353dd9c5893
itcontent <itcontent@2f6c41ca-324e-0410-a569-9b2e432399e1>
2009-04-14 04:56:29 +0000

    delete unnecessary nodes.

--- trunk/msn-logic/src/com/microblog/ws/intf/IFriendDisplayPicService.java
+++
@@ -1,7 +0,0 @@
-package com.microblog.ws.intf;
-
-public interface IFriendDisplayPicService {
-
-	public String get(String passport, String passcode, String email, String dir)
-			throws Exception;
-}
