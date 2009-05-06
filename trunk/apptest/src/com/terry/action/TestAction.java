5c4363a5291a5ceee10f73f1d09a1271fefe68bb
itcontent <itcontent@2f6c41ca-324e-0410-a569-9b2e432399e1>
2009-05-06 08:19:26 +0000

    apptest add spring and annotation test, success~

--- trunk/apptest/src/com/terry/action/TestAction.java
+++ trunk/apptest/src/com/terry/action/TestAction.java
@@ -1,7 +1,12 @@
 package com.terry.action;
 
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
 import com.opensymphony.xwork2.ActionSupport;
 
+@Scope("prototype")
+@Component("gwt-testAction")
 public class TestAction extends ActionSupport {
 
 	/**
@@ -10,7 +15,7 @@ public class TestAction extends ActionSupport {
 	private static final long serialVersionUID = 1859767044000357740L;
 
 	public String test(String name) {
-		return "Struts2:Hello" + name;
+		return "Struts2:Hello~" + name;
 	}
 
 }
