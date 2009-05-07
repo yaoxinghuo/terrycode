d6fe8cd33abccb08db058ffc4926ccff17fa060b
itcontent <itcontent@2f6c41ca-324e-0410-a569-9b2e432399e1>
2009-05-07 07:05:52 +0000

    apptest add struts2 action.

--- apptest/src/com/terry/action/IndexAction.java
+++ apptest/src/com/terry/action/IndexAction.java
@@ -14,9 +14,20 @@ public class IndexAction extends ActionSupport {
 	 */
 	private static final long serialVersionUID = 4575781393306576158L;
 
+	private String message;
+
 	@Override
 	public String execute() {
+		setMessage("struts2 action 传过来的中文！");
 		return SUCCESS;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public String getMessage() {
+		return message;
 	}
 
 }
