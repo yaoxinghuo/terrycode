2a0bce2a8e2ea667fd26cbd8dc1dc1865658026f
itcontent <itcontent@2f6c41ca-324e-0410-a569-9b2e432399e1>
2009-05-15 09:59:08 +0000

    apptest layout test.

--- apptest/src/com/terry/client/Apptest.java
+++ apptest/src/com/terry/client/Apptest.java
@@ -4,19 +4,29 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.extjs.gxt.ui.client.Style.HorizontalAlignment;
+import com.extjs.gxt.ui.client.Style.LayoutRegion;
+import com.extjs.gxt.ui.client.Style.Scroll;
 import com.extjs.gxt.ui.client.data.BasePagingLoader;
 import com.extjs.gxt.ui.client.data.ModelData;
 import com.extjs.gxt.ui.client.data.ModelType;
 import com.extjs.gxt.ui.client.data.PagingLoadResult;
 import com.extjs.gxt.ui.client.store.ListStore;
+import com.extjs.gxt.ui.client.util.Margins;
 import com.extjs.gxt.ui.client.widget.ContentPanel;
+import com.extjs.gxt.ui.client.widget.HtmlContainer;
+import com.extjs.gxt.ui.client.widget.TabItem;
+import com.extjs.gxt.ui.client.widget.TabPanel;
 import com.extjs.gxt.ui.client.widget.Viewport;
 import com.extjs.gxt.ui.client.widget.grid.ColumnConfig;
 import com.extjs.gxt.ui.client.widget.grid.ColumnModel;
 import com.extjs.gxt.ui.client.widget.grid.Grid;
+import com.extjs.gxt.ui.client.widget.layout.AccordionLayout;
+import com.extjs.gxt.ui.client.widget.layout.BorderLayout;
+import com.extjs.gxt.ui.client.widget.layout.BorderLayoutData;
 import com.extjs.gxt.ui.client.widget.layout.FitLayout;
-import com.extjs.gxt.ui.client.widget.layout.MarginData;
 import com.extjs.gxt.ui.client.widget.toolbar.PagingToolBar;
+import com.extjs.gxt.ui.client.widget.tree.Tree;
+import com.extjs.gxt.ui.client.widget.tree.TreeItem;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
@@ -53,10 +63,11 @@ public class Apptest implements EntryPoint {
 	private final GreetingServiceAsync greetingService = GWT
 			.create(GreetingService.class);
 
+	private Viewport viewport;
+
 	/**
 	 * This is the entry point method.
 	 */
-	@SuppressWarnings("unchecked")
 	public void onModuleLoad() {
 
 		MyConstants constants = GWT.create(MyConstants.class);
@@ -162,6 +173,102 @@ public class Apptest implements EntryPoint {
 		sendButton.addClickHandler(handler);
 		nameField.addKeyUpHandler(handler);
 
+		
+
+		viewport = new Viewport();
+		viewport.setLayout(new BorderLayout());
+
+		createNorth();
+		createWest();
+		createCenter();
+		
+		RootPanel.get().add(viewport);
+
+	}
+
+	private void createNorth() {
+		StringBuffer sb = new StringBuffer();
+		sb
+				.append("<div id='demo-header' class='x-small-editor'><div id='demo-theme'></div><div id=demo-title>Ext GWT Examples</div></div>");
+
+		HtmlContainer northPanel = new HtmlContainer(sb.toString());
+		northPanel.setEnableState(false);
+
+		BorderLayoutData northData = new BorderLayoutData(LayoutRegion.NORTH,
+				33);
+		northData.setMargins(new Margins(0,2,0,0));
+		viewport.add(northPanel, northData);
+	}
+
+	private void createWest() {
+		ContentPanel panel = new ContentPanel();
+		panel.setHeading("AccordionLayout");
+		panel.setBodyBorder(false);
+
+		panel.setLayout(new AccordionLayout());
+		panel.setIconStyle("icon-accordion");
+
+		ContentPanel cp = new ContentPanel();
+		cp.setHeading("Online Users");
+		cp.setScrollMode(Scroll.AUTO);
+		panel.add(cp);
+
+		Tree tree = new Tree();
+		TreeItem family = new TreeItem("Family");
+		tree.getRootItem().add(family);
+		family.add(newItem("Darrell", "user"));
+		family.add(newItem("Maro", "user-girl"));
+		family.add(newItem("Lia", "user-kid"));
+		family.add(newItem("Alec", "user-kid"));
+		family.add(newItem("Andrew", "user-kid"));
+		family.setExpanded(true);
+
+		TreeItem friends = new TreeItem("Friends");
+		tree.getRootItem().add(friends);
+		friends.add(newItem("Bob", "user"));
+		friends.add(newItem("Mary", "user-girl"));
+		friends.add(newItem("Sally", "user-girl"));
+		friends.add(newItem("Jack", "user"));
+		friends.setExpanded(true);
+
+		cp.add(tree);
+
+		cp = new ContentPanel();
+		cp.setBodyStyleName("pad-text");
+		cp.setHeading("Settings");
+		cp.addText("Test3");
+		panel.add(cp);
+
+		cp = new ContentPanel();
+		cp.setBodyStyleName("pad-text");
+		cp.setHeading("Stuff");
+		cp.addText("Test2");
+		panel.add(cp);
+
+		cp = new ContentPanel();
+		cp.setBodyStyleName("pad-text");
+		cp.setHeading("More Stuff");
+		cp.addText("Test1");
+		panel.add(cp);
+		panel.setWidth(200);
+
+		BorderLayoutData westData = new BorderLayoutData(LayoutRegion.WEST, 200);
+		westData.setSplit(true);
+		westData.setCollapsible(true);
+		westData.setMargins(new Margins(0,5,0,0));
+
+		viewport.add(panel, westData);
+
+	}
+
+	private TreeItem newItem(String text, String iconStyle) {
+		TreeItem item = new TreeItem(text);
+		item.setIconStyle(iconStyle);
+		return item;
+	}
+	
+	@SuppressWarnings("unchecked")
+	private void createCenter(){
 		List<ColumnConfig> configs = new ArrayList<ColumnConfig>();
 
 		ColumnConfig column = new ColumnConfig();
@@ -197,28 +304,32 @@ public class Apptest implements EntryPoint {
 		// --------------- //
 		ListStore<ModelData> store = DataStruction.JsonStoreCreatePaginate(
 				"ds01", mt, "/ajax/grid.action", "id"); // this will register
-														// into ds01
+		// into ds01
 
 		ColumnModel cm = new ColumnModel(configs);
 
 		ContentPanel cp = new ContentPanel();
 		cp.setBodyBorder(false);
-		cp.setHeading("Basic Grid");
+		cp.setHeaderVisible(true);
 		cp.setButtonAlign(HorizontalAlignment.CENTER);
 		cp.setLayout(new FitLayout());
 		Grid<ModelData> grid = new Grid<ModelData>(store, cm);
 		grid.setStyleAttribute("borderTop", "none");
 		grid.setAutoExpandColumn("name");
-		grid.setBorders(true);
 		cp.add(grid);
 		PagingToolBar toolBar = new PagingToolBar(20);
 		toolBar.bind((BasePagingLoader<PagingLoadResult<ModelData>>) store
 				.getLoader());
 		cp.setBottomComponent(toolBar);
-
-		Viewport viewport = new Viewport();
-		viewport.add(cp, new MarginData(10));
-		RootPanel.get().add(viewport);
-
+		
+		TabPanel tp = new TabPanel();
+		tp.setBodyBorder(false);
+		TabItem item = new TabItem();  
+	    item.setText("Grid Tab"); 
+	    item.setScrollMode(Scroll.AUTO);
+	    item.add(cp);
+	    tp.add(item);
+		
+		viewport.add(tp, new BorderLayoutData(LayoutRegion.CENTER));
 	}
 }
