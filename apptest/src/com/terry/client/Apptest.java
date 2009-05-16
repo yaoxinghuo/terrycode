fa2a4f85763af888b24ecbabd31908426ac017cc
yaoxinghuo <yaoxinghuo@2f6c41ca-324e-0410-a569-9b2e432399e1>
2009-05-16 04:25:23 +0000

    apptest gxt grid test updated.

--- apptest/src/com/terry/client/Apptest.java
+++ apptest/src/com/terry/client/Apptest.java
@@ -6,6 +6,7 @@ import java.util.List;
 import com.extjs.gxt.ui.client.Style.HorizontalAlignment;
 import com.extjs.gxt.ui.client.Style.LayoutRegion;
 import com.extjs.gxt.ui.client.Style.Scroll;
+import com.extjs.gxt.ui.client.Style.SelectionMode;
 import com.extjs.gxt.ui.client.data.BasePagingLoader;
 import com.extjs.gxt.ui.client.data.ModelData;
 import com.extjs.gxt.ui.client.data.ModelType;
@@ -17,171 +18,36 @@ import com.extjs.gxt.ui.client.widget.HtmlContainer;
 import com.extjs.gxt.ui.client.widget.TabItem;
 import com.extjs.gxt.ui.client.widget.TabPanel;
 import com.extjs.gxt.ui.client.widget.Viewport;
+import com.extjs.gxt.ui.client.widget.button.ToolButton;
+import com.extjs.gxt.ui.client.widget.grid.CheckBoxSelectionModel;
 import com.extjs.gxt.ui.client.widget.grid.ColumnConfig;
 import com.extjs.gxt.ui.client.widget.grid.ColumnModel;
 import com.extjs.gxt.ui.client.widget.grid.Grid;
+import com.extjs.gxt.ui.client.widget.grid.RowNumberer;
 import com.extjs.gxt.ui.client.widget.layout.AccordionLayout;
 import com.extjs.gxt.ui.client.widget.layout.BorderLayout;
 import com.extjs.gxt.ui.client.widget.layout.BorderLayoutData;
 import com.extjs.gxt.ui.client.widget.layout.FitLayout;
+import com.extjs.gxt.ui.client.widget.menu.Menu;
+import com.extjs.gxt.ui.client.widget.menu.MenuItem;
 import com.extjs.gxt.ui.client.widget.toolbar.PagingToolBar;
+import com.extjs.gxt.ui.client.widget.toolbar.ToolBar;
 import com.extjs.gxt.ui.client.widget.tree.Tree;
 import com.extjs.gxt.ui.client.widget.tree.TreeItem;
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.core.client.GWT;
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.event.dom.client.KeyCodes;
-import com.google.gwt.event.dom.client.KeyUpEvent;
-import com.google.gwt.event.dom.client.KeyUpHandler;
-import com.google.gwt.user.client.rpc.AsyncCallback;
-import com.google.gwt.user.client.rpc.ServiceDefTarget;
-import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.DialogBox;
-import com.google.gwt.user.client.ui.HTML;
-import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
-import com.google.gwt.user.client.ui.TextBox;
-import com.google.gwt.user.client.ui.VerticalPanel;
 
-/**
- * Entry point classes define <code>onModuleLoad()</code>.
- */
 public class Apptest implements EntryPoint {
-	/**
-	 * The message displayed to the user when the server cannot be reached or
-	 * returns an error.
-	 */
-	private static final String SERVER_ERROR = "An error occurred while "
-			+ "attempting to contact the server. Please check your network "
-			+ "connection and try again.";
-
-	/**
-	 * Create a remote service proxy to talk to the server-side Greeting
-	 * service.
-	 */
-	private final GreetingServiceAsync greetingService = GWT
-			.create(GreetingService.class);
-
 	private Viewport viewport;
 
-	/**
-	 * This is the entry point method.
-	 */
 	public void onModuleLoad() {
-
-		MyConstants constants = GWT.create(MyConstants.class);
-		final Button sendButton = new Button(constants.btSend());
-		final TextBox nameField = new TextBox();
-		nameField.setText("GWT User");
-
-		// We can add style names to widgets
-		sendButton.addStyleName("sendButton");
-
-		// Add the nameField and sendButton to the RootPanel
-		// Use RootPanel.get() to get the entire body element
-		RootPanel.get("nameFieldContainer").add(nameField);
-		RootPanel.get("sendButtonContainer").add(sendButton);
-
-		// Focus the cursor on the name field when the app loads
-		nameField.setFocus(true);
-		nameField.selectAll();
-
-		// Create the popup dialog box
-		final DialogBox dialogBox = new DialogBox();
-		dialogBox.setText("Remote Procedure Call");
-		dialogBox.setAnimationEnabled(true);
-		final Button closeButton = new Button("Close");
-		// We can set the id of a widget by accessing its Element
-		closeButton.getElement().setId("closeButton");
-		final Label textToServerLabel = new Label();
-		final HTML serverResponseLabel = new HTML();
-		VerticalPanel dialogVPanel = new VerticalPanel();
-		dialogVPanel.addStyleName("dialogVPanel");
-		dialogVPanel.add(new HTML("<b>Sending name to the server:</b>"));
-		dialogVPanel.add(textToServerLabel);
-		dialogVPanel.add(new HTML(constants.reply()));
-		dialogVPanel.add(serverResponseLabel);
-		dialogVPanel.setHorizontalAlignment(VerticalPanel.ALIGN_RIGHT);
-		dialogVPanel.add(closeButton);
-		dialogBox.setWidget(dialogVPanel);
-
-		// Add a handler to close the DialogBox
-		closeButton.addClickHandler(new ClickHandler() {
-			public void onClick(ClickEvent event) {
-				dialogBox.hide();
-				sendButton.setEnabled(true);
-				sendButton.setFocus(true);
-			}
-		});
-
-		// Create a handler for the sendButton and nameField
-		class MyHandler implements ClickHandler, KeyUpHandler {
-			/**
-			 * Fired when the user clicks on the sendButton.
-			 */
-			public void onClick(ClickEvent event) {
-				sendNameToServer();
-			}
-
-			/**
-			 * Fired when the user types in the nameField.
-			 */
-			public void onKeyUp(KeyUpEvent event) {
-				if (event.getNativeKeyCode() == KeyCodes.KEY_ENTER) {
-					sendNameToServer();
-				}
-			}
-
-			/**
-			 * Send the name from the nameField to the server and wait for a
-			 * response.
-			 */
-			private void sendNameToServer() {
-				sendButton.setEnabled(false);
-				String textToServer = nameField.getText();
-				textToServerLabel.setText(textToServer);
-				serverResponseLabel.setText("");
-				ServiceDefTarget endpoint = (ServiceDefTarget) greetingService;
-				endpoint.setServiceEntryPoint("gwt-test.action");
-				greetingService.test(textToServer, new AsyncCallback<String>() {
-					public void onFailure(Throwable caught) {
-						// Show the RPC error message to the user
-						dialogBox.setText("Remote Procedure Call - Failure");
-						serverResponseLabel
-								.addStyleName("serverResponseLabelError");
-						serverResponseLabel.setHTML(SERVER_ERROR);
-						dialogBox.center();
-						closeButton.setFocus(true);
-					}
-
-					public void onSuccess(String result) {
-						dialogBox.setText("Remote Procedure Call");
-						serverResponseLabel
-								.removeStyleName("serverResponseLabelError");
-						MyMessages message = GWT.create(MyMessages.class);
-						serverResponseLabel.setHTML(message.reply(result));
-						dialogBox.center();
-						closeButton.setFocus(true);
-					}
-				});
-			}
-		}
-
-		// Add a handler to send the name to the server
-		MyHandler handler = new MyHandler();
-		sendButton.addClickHandler(handler);
-		nameField.addKeyUpHandler(handler);
-
-		
-
 		viewport = new Viewport();
 		viewport.setLayout(new BorderLayout());
 
 		createNorth();
 		createWest();
 		createCenter();
-		
+
 		RootPanel.get().add(viewport);
 
 	}
@@ -196,7 +62,7 @@ public class Apptest implements EntryPoint {
 
 		BorderLayoutData northData = new BorderLayoutData(LayoutRegion.NORTH,
 				33);
-		northData.setMargins(new Margins(0,2,0,0));
+		northData.setMargins(new Margins(0, 2, 0, 0));
 		viewport.add(northPanel, northData);
 	}
 
@@ -255,7 +121,7 @@ public class Apptest implements EntryPoint {
 		BorderLayoutData westData = new BorderLayoutData(LayoutRegion.WEST, 200);
 		westData.setSplit(true);
 		westData.setCollapsible(true);
-		westData.setMargins(new Margins(0,5,0,0));
+		westData.setMargins(new Margins(0, 5, 0, 0));
 
 		viewport.add(panel, westData);
 
@@ -266,10 +132,17 @@ public class Apptest implements EntryPoint {
 		item.setIconStyle(iconStyle);
 		return item;
 	}
-	
+
 	@SuppressWarnings("unchecked")
-	private void createCenter(){
+	private void createCenter() {
 		List<ColumnConfig> configs = new ArrayList<ColumnConfig>();
+
+		RowNumberer r = new RowNumberer();
+		configs.add(r);
+
+		CheckBoxSelectionModel<ModelData> sm = new CheckBoxSelectionModel<ModelData>();
+		sm.setSelectionMode(SelectionMode.MULTI);
+		configs.add(sm.getColumn());
 
 		ColumnConfig column = new ColumnConfig();
 		column.setId("name");
@@ -301,35 +174,44 @@ public class Apptest implements EntryPoint {
 		mt.addField("name");
 		mt.addField("symbol");
 
+		ColumnModel cm = new ColumnModel(configs);
+
 		// --------------- //
 		ListStore<ModelData> store = DataStruction.JsonStoreCreatePaginate(
 				"ds01", mt, "/ajax/grid.action", "id"); // this will register
 		// into ds01
 
-		ColumnModel cm = new ColumnModel(configs);
-
 		ContentPanel cp = new ContentPanel();
 		cp.setBodyBorder(false);
-		cp.setHeaderVisible(true);
+		cp.setHeaderVisible(false);
 		cp.setButtonAlign(HorizontalAlignment.CENTER);
 		cp.setLayout(new FitLayout());
 		Grid<ModelData> grid = new Grid<ModelData>(store, cm);
-		grid.setStyleAttribute("borderTop", "none");
+		grid.setSelectionModel(sm);
+		grid.addPlugin(sm);
 		grid.setAutoExpandColumn("name");
+		grid.setHeight(480);
+		Menu menu = new Menu();
+		menu.add(new MenuItem("Test"));
+		menu.add(new MenuItem("Test2"));
+		grid.setContextMenu(menu);
 		cp.add(grid);
+		ToolBar tb = new ToolBar();
+		tb.add(new ToolButton("x-tool-gear"));
+		tb.add(new ToolButton("x-tool-close"));
+		cp.setTopComponent(tb);
 		PagingToolBar toolBar = new PagingToolBar(20);
 		toolBar.bind((BasePagingLoader<PagingLoadResult<ModelData>>) store
 				.getLoader());
 		cp.setBottomComponent(toolBar);
-		
+
 		TabPanel tp = new TabPanel();
-		tp.setBodyBorder(false);
-		TabItem item = new TabItem();  
-	    item.setText("Grid Tab"); 
-	    item.setScrollMode(Scroll.AUTO);
-	    item.add(cp);
-	    tp.add(item);
-		
+		TabItem item = new TabItem();
+		item.setText("Grid Tab");
+		item.setScrollMode(Scroll.AUTO);
+		item.add(cp);
+		tp.add(item);
+
 		viewport.add(tp, new BorderLayoutData(LayoutRegion.CENTER));
 	}
 }
