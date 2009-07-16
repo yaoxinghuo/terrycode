3fdc4021ae710536863075946b46be082a0b8a1a
itcontent <itcontent@2f6c41ca-324e-0410-a569-9b2e432399e1>
2009-07-16 03:22:27 +0000

    costnote completely remove session support.

--- costnote/src/com/terry/costnote/action/GenericAction.java
+++ costnote/src/com/terry/costnote/action/GenericAction.java
@@ -1,34 +1,22 @@
 package com.terry.costnote.action;
 
-import java.util.Map;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.struts2.interceptor.ServletRequestAware;
-import org.apache.struts2.interceptor.SessionAware;
 
 import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
 import com.opensymphony.xwork2.ActionSupport;
 
-public class GenericAction extends ActionSupport implements SessionAware,
-		ServletRequestAware {
+public class GenericAction extends ActionSupport implements ServletRequestAware {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 6076893161927391902L;
-	protected Map<Object, Object> sessionMap;
-
 	protected HttpServletRequest request;
 	protected HttpServletResponse response;
-
-	@SuppressWarnings("unchecked")
-	@Override
-	public void setSession(Map session) {
-		sessionMap = session;
-	}
 
 	@Override
 	public void setServletRequest(HttpServletRequest request) {
