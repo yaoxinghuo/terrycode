35cb83ed1663558a59c2feec10d994e1a98bc72e
itcontent <itcontent@2f6c41ca-324e-0410-a569-9b2e432399e1>
2009-08-05 08:05:20 +0000

    costnote because of javax.naming.NamingException, I have to revert.

--- costnote/src/com/terry/costnote/data/dao/impl/AccountDaoImpl.java
+++ costnote/src/com/terry/costnote/data/dao/impl/AccountDaoImpl.java
@@ -3,8 +3,8 @@ package com.terry.costnote.data.dao.impl;
 import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
 import javax.persistence.NoResultException;
-import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 
 import org.springframework.stereotype.Component;
@@ -13,6 +13,7 @@ import com.google.appengine.api.datastore.Key;
 import com.google.appengine.api.datastore.KeyFactory;
 import com.terry.costnote.data.dao.intf.IAccountDao;
 import com.terry.costnote.data.model.Account;
+import com.terry.costnote.data.util.EMF;
 
 /**
  * @author Terry E-mail: yaoxinghuo at 126 dot com
@@ -22,60 +23,65 @@ import com.terry.costnote.data.model.Account;
 @Component("accountDao")
 public class AccountDaoImpl implements IAccountDao {
 
-	@PersistenceContext
-	private EntityManager em;
+        EntityManager em = EMF.get().createEntityManager();
 
-	@Override
-	public boolean deleteAccount(Account account) {
-		try {
-			em.remove(account);
-		} catch (Exception e) {
-			return false;
-		}
-		return true;
-	}
+        @Override
+        public boolean deleteAccount(Account account) {
+                try {
+                        EntityTransaction tx = em.getTransaction();
+                        tx.begin();
+                        em.remove(account);
+                        tx.commit();
+                } catch (Exception e) {
+                        return false;
+                }
+                return true;
+        }
 
-	@Override
-	public Account getAccountByEmail(String email) {
-		Query query = em.createQuery("SELECT a FROM " + Account.class.getName()
-				+ " a where a.email=:email");
-		query.setParameter("email", email);
-		Account account = null;
-		try {
-			account = (Account) query.getSingleResult();
-		} catch (NoResultException e) {
-		}
-		return account;
-	}
+        @Override
+        public Account getAccountByEmail(String email) {
+                Query query = em.createQuery("SELECT a FROM " + Account.class.getName()
+                                + " a where a.email=:email");
+                query.setParameter("email", email);
+                Account account = null;
+                try {
+                        account = (Account) query.getSingleResult();
+                } catch (NoResultException e) {
+                }
+                return account;
+        }
 
-	@Override
-	public Account getAccountById(String accountId) {
-		Key key = KeyFactory.stringToKey(accountId);
-		if (key == null || !key.isComplete())
-			return null;
+        @Override
+        public Account getAccountById(String accountId) {
+                Key key = KeyFactory.stringToKey(accountId);
+                if (key == null || !key.isComplete())
+                        return null;
 
-		return em.find(Account.class, key);
-	}
+                return em.find(Account.class, key);
+        }
 
-	@SuppressWarnings("unchecked")
-	@Override
-	public List<Account> getAccounts(int start, int limit) {
-		Query query = em.createQuery("SELECT a FROM " + Account.class.getName()
-				+ " a");
-		query.setFirstResult(start);
-		if (limit != 0)
-			query.setMaxResults(limit);
-		return query.getResultList();
-	}
+        @SuppressWarnings("unchecked")
+        @Override
+        public List<Account> getAccounts(int start, int limit) {
+                Query query = em.createQuery("SELECT a FROM " + Account.class.getName()
+                                + " a");
+                query.setFirstResult(start);
+                if (limit != 0)
+                        query.setMaxResults(limit);
+                return query.getResultList();
+        }
 
-	@Override
-	public boolean saveAccount(Account account) {
-		try {
-			em.persist(account);
-		} catch (Exception e) {
-			return false;
-		}
-		return true;
-	}
+        @Override
+        public boolean saveAccount(Account account) {
+                try {
+                        EntityTransaction tx = em.getTransaction();
+                        tx.begin();
+                        em.persist(account);
+                        tx.commit();
+                } catch (Exception e) {
+                        return false;
+                }
+                return true;
+        }
 
 }
