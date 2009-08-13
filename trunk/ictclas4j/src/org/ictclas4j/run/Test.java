93e36f3cf1efcc8e69b3ffe18363793a41ff9ef6
itcontent <itcontent@2f6c41ca-324e-0410-a569-9b2e432399e1>
2009-08-13 07:02:54 +0000

    ictclas4j initial import. copy from code.google.com/p/ictclas4j

---
+++ trunk/ictclas4j/src/org/ictclas4j/run/Test.java
@@ -0,0 +1,11 @@
+package org.ictclas4j.run;
+
+/**
+ * @author xinghuo.yao E-mail: yaoxinghuo at 126 dot com
+ * @version create£ºAug 13, 2009 2:47:40 PM
+ */
+public class Test {
+	public static void main(String[] args) {
+		
+	}
+}
