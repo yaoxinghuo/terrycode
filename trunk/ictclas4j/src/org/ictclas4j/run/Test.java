8424b4282f30d76c9028a118bb527f77dba91e25
itcontent <itcontent@2f6c41ca-324e-0410-a569-9b2e432399e1>
2009-08-14 03:33:31 +0000

    ictclas4j update test.

--- trunk/ictclas4j/src/org/ictclas4j/run/Test.java
+++ trunk/ictclas4j/src/org/ictclas4j/run/Test.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import org.ictclas4j.bean.SegResult;
 import org.ictclas4j.bean.WordResultBean;
 import org.ictclas4j.segment.Segment;
+import org.ictclas4j.utility.Chineses;
 
 /**
  * @author xinghuo.yao E-mail: yaoxinghuo at 126 dot com
@@ -14,12 +15,20 @@ public class Test {
 	public static void main(String[] args) {
 		Segment segTag = Segment.getInstance(1);
 		try {
-			SegResult seg_res = segTag.split("今天的天氣好極了");
-			ArrayList<WordResultBean> words = seg_res.getResult();
-			for(WordResultBean word:words){
-				System.out.println(word.getWord()+"/"+word.getProperty());
+			StringBuffer sb = new StringBuffer("");
+			String input = "我購買了道具和服裝";
+			SegResult seg_res = segTag.split(input);
+			ArrayList<WordResultBean> results = seg_res.getResult();
+			for (WordResultBean result : results) {
+				if (!input.contains(result.getWord()))
+					sb.append(Chineses.toFan(result.getWord()));
+				else
+					sb.append(result.getWord());
+				sb.append("/" + result.getProperty()).append(" ");
 			}
+
 			String segString = seg_res.getFinalResult();
+			System.out.println(sb.toString());
 			System.out.println("--------------");
 			System.out.println(segString);
 		} catch (Throwable t) {
