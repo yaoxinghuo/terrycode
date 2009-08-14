282842b3a5f6a517e88abdd9e964919581e8c40f
itcontent <itcontent@2f6c41ca-324e-0410-a569-9b2e432399e1>
2009-08-14 03:44:08 +0000

    ictclas4j update test.

--- ictclas4j/src/org/ictclas4j/run/Test.java
+++ ictclas4j/src/org/ictclas4j/run/Test.java
@@ -12,27 +12,23 @@ import org.ictclas4j.utility.Chineses;
  * @version create：Aug 13, 2009 2:47:40 PM
  */
 public class Test {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		Segment segTag = Segment.getInstance(1);
-		try {
-			StringBuffer sb = new StringBuffer("");
-			String input = "我購買了道具和服裝";
-			SegResult seg_res = segTag.split(input);
-			ArrayList<WordResultBean> results = seg_res.getResult();
-			for (WordResultBean result : results) {
-				if (!input.contains(result.getWord()))
-					sb.append(Chineses.toFan(result.getWord()));
-				else
-					sb.append(result.getWord());
-				sb.append("/" + result.getProperty()).append(" ");
-			}
-
-			String segString = seg_res.getFinalResult();
-			System.out.println(sb.toString());
-			System.out.println("--------------");
-			System.out.println(segString);
-		} catch (Throwable t) {
-			t.printStackTrace();
+		StringBuffer sb = new StringBuffer("");
+		String input = "我購買了道具和服裝";
+		SegResult seg_res = segTag.split(input);
+		ArrayList<WordResultBean> results = seg_res.getResult();
+		for (WordResultBean result : results) {
+			if (!input.contains(result.getWord()))
+				sb.append(Chineses.toFan(result.getWord()));
+			else
+				sb.append(result.getWord());
+			sb.append("/" + result.getProperty()).append(" ");
 		}
+
+		String segString = seg_res.getFinalResult();
+		System.out.println(sb.toString());
+		System.out.println("--------------");
+		System.out.println(segString);
 	}
 }
