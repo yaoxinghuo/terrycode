4df1b30a584ac85436128431f58c049cd562b9ac
itcontent <itcontent@2f6c41ca-324e-0410-a569-9b2e432399e1>
2009-08-20 04:44:23 +0000

    gfwout add a filter to ignore the resource that do not exist on server.

---
+++ gfwout/src/com/terry/gfwout/filter/GfwFilter.java
@@ -0,0 +1,50 @@
+package com.terry.gfwout.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author xinghuo.yao E-mail: yaoxinghuo at 126 dot com
+ * @version create：Aug 20, 2009 12:13:47 PM
+ */
+public class GfwFilter implements Filter {
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void doFilter(ServletRequest arg0, ServletResponse arg1,
+			FilterChain arg2) throws IOException, ServletException {
+		String uri = ((HttpServletRequest) arg0).getRequestURI();
+		if (isAvaliable(uri))
+			arg2.doFilter(arg0, arg1);
+	}
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+		// TODO Auto-generated method stub
+
+	}
+
+	private boolean isAvaliable(String uri) {
+		if (uri == null || uri.equals(""))
+			return false;
+		String[] as = { "/", "/router", "/gfw" };
+		for (String a : as) {
+			if (uri.equals(a))
+				return true;
+		}
+		return false;
+	}
+
+}
