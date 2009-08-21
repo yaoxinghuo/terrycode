ab6b5042b1f1c6fb4367304956502c29f734297a
itcontent <itcontent@2f6c41ca-324e-0410-a569-9b2e432399e1>
2009-08-21 03:51:05 +0000

    gfwout add function to scroll the back layer when user scroll window, updated and change the baseurl depend on server or dec env.

--- gfwout/src/com/terry/gfwout/util/StringUtil.java
+++ gfwout/src/com/terry/gfwout/util/StringUtil.java
@@ -167,12 +167,14 @@ public class StringUtil {
 			StringBuffer sb2 = new StringBuffer(matcher2.group());
 			int p = sb2.indexOf(">");
 			if (p != -1) {
-				sb2.insert(p + 1, Constants.OTHER_HTML
+				sb2.insert(p + 1, Constants.ANALYTICS_HTML
 						+ Constants.BACK_HOME_HTML);
 			}
 			matcher2.appendReplacement(sb, sb2.toString());
 		}
 		matcher2.appendTail(sb);
+
+		sb.append(Constants.SCRIPT_HTML);
 		return sb.toString();
 	}
 
