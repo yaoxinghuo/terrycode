d33acf231b839cf93e7b5f0eb93f732923f04727
itcontent <itcontent@2f6c41ca-324e-0410-a569-9b2e432399e1>
2009-10-19 06:34:55 +0000

    fetionlib update Text.java

--- ictclas4j/src/org/ictclas4j/run/Test3.java
+++ ictclas4j/src/org/ictclas4j/run/Test3.java
@@ -1,6 +1,9 @@
 package org.ictclas4j.run;
 
+import java.util.TreeMap;
+
 import org.ictclas4j.bean.SegResult;
+import org.ictclas4j.bean.WordResultBean;
 import org.ictclas4j.segment.Segment;
 
 /**
@@ -8,9 +11,22 @@ import org.ictclas4j.segment.Segment;
  * @version create：Sep 22, 2009 12:40:58 PM
  */
 public class Test3 {
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) {
 		Segment segTag = Segment.getInstance(1);
-		SegResult seg_res = segTag.split("종합送與您網站內容相關的廣告，讓您網站的收益潛能最大化");
+		SegResult seg_res = segTag.split("第一件T恤");
 		System.out.println("Raw Result:\n" + seg_res.getFinalResult());
+		TreeMap<Integer, WordResultBean> results = seg_res.getPosResult();
+		StringBuffer sb = new StringBuffer("");
+		for (WordResultBean result : results.values()) {
+			if (result == null)
+				continue;
+			sb.append(result.getWord());
+			sb.append("/" + result.getProperty());
+
+			if (result.isStopWord())
+				sb.append("[s]");
+			sb.append(" ");
+		}
+		System.out.println("Optimized Results:\n" + sb);
 	}
 }
