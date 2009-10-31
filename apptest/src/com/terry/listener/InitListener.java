c2ecc3657a43f709197a6286277b68db5cf8e574
yaoxinghuo <yaoxinghuo@2f6c41ca-324e-0410-a569-9b2e432399e1>
2009-10-31 15:42:07 +0000

    apptest add funtunetelling function to wave robot for testing.

--- apptest/src/com/terry/listener/InitListener.java
+++ apptest/src/com/terry/listener/InitListener.java
@@ -1,21 +1,86 @@
 package com.terry.listener;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.ArrayList;
+
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
+import jxl.Cell;
+import jxl.Sheet;
+import jxl.Workbook;
 import ognl.OgnlRuntime;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import com.terry.robot.logic.Fortune;
+import com.terry.robot.logic.FortuneContent;
 
 public class InitListener implements ServletContextListener {
 
+	private static Log log = LogFactory.getLog(InitListener.class);
+
 	@Override
 	public void contextDestroyed(ServletContextEvent arg0) {
-		// TODO Auto-generated method stub
-
 	}
 
 	@Override
-	public void contextInitialized(ServletContextEvent arg0) {
+	public void contextInitialized(ServletContextEvent contextEvent) {
 		OgnlRuntime.setSecurityManager(null);
+
+		String basePath = contextEvent.getServletContext().getRealPath("/");
+		try {
+			ArrayList<Fortune> fs = new ArrayList<Fortune>();
+
+			Workbook rwb = Workbook.getWorkbook(new FileInputStream(new File(
+					basePath + File.separator + "WEB-INF" + File.separator
+							+ "fortune.xls")));
+			Sheet rs = rwb.getSheet(0);
+			for (int i = 0; i < rs.getRows(); i++) {
+				Fortune fortune = new Fortune();
+				for (int j = 0; j < rs.getColumns(); j++) {
+					Cell cell = rs.getCell(j, i);
+					String value = cell.getContents();
+					switch (j) {
+					case 0:
+						fortune.setCol0(value);
+						fortune.setNo(i + 1);
+						fortune.setId(String.valueOf(i + 1));
+						break;
+					case 1:
+						fortune.setCol1(value);
+						break;
+					case 2:
+						fortune.setCol2(value);
+						break;
+					case 3:
+						fortune.setCol3(value);
+						break;
+					case 4:
+						fortune.setCol4(value);
+						break;
+					case 5:
+						fortune.setCol5(value);
+						break;
+					case 6:
+						fortune.setCol6(value);
+						break;
+					case 7:
+						fortune.setCol7(value);
+						break;
+					}
+				}
+
+				fs.add(fortune);
+			}
+
+			FortuneContent.fortunes = fs;
+
+		} catch (Exception e) {
+			log.error("can not load fortune.json. exception:" + e.getMessage());
+		}
 	}
 
 }
--- apptest/src/com/terry/servlet/WaveReceiverServlet.java
+++ apptest/src/com/terry/servlet/WaveReceiverServlet.java
@@ -1,8 +1,19 @@
 package com.terry.servlet;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.cache.Cache;
+import javax.cache.CacheException;
+import javax.cache.CacheManager;
+import javax.servlet.ServletException;
+
+import net.sf.json.JSONObject;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
+import com.google.appengine.api.memcache.stdimpl.GCacheFactory;
 import com.google.wave.api.AbstractRobotServlet;
 import com.google.wave.api.Blip;
 import com.google.wave.api.Event;
@@ -10,6 +21,8 @@ import com.google.wave.api.EventType;
 import com.google.wave.api.RobotMessageBundle;
 import com.google.wave.api.TextView;
 import com.google.wave.api.Wavelet;
+import com.terry.robot.logic.FortuneContent;
+import com.terry.robot.logic.FortunetellingLogic;
 
 /**
  * @author Terry E-mail: yaoxinghuo at 126 dot com
@@ -24,6 +37,23 @@ public class WaveReceiverServlet extends AbstractRobotServlet {
 
 	private static Log log = LogFactory.getLog(WaveReceiverServlet.class);
 
+	private Cache cache;
+
+	private FortunetellingLogic fortunetellingLogic = new FortunetellingLogic();
+
+	@Override
+	public void init() throws ServletException {
+		super.init();
+		Map<Integer, Integer> CACHE_PROPS = new HashMap<Integer, Integer>();
+		CACHE_PROPS.put(GCacheFactory.EXPIRATION_DELTA, 60 * 10);
+		try {
+			cache = CacheManager.getInstance().getCacheFactory().createCache(
+					CACHE_PROPS);
+		} catch (CacheException e) {
+		}
+		fortunetellingLogic.setFortunes(FortuneContent.fortunes);
+	}
+
 	@Override
 	public void processEvents(RobotMessageBundle bundle) {
 		log.debug("wave event received...");
@@ -32,17 +62,76 @@ public class WaveReceiverServlet extends AbstractRobotServlet {
 		if (bundle.wasSelfAdded()) {
 			Blip blip = wavelet.appendBlip();
 			TextView textView = blip.getDocument();
-			textView.append("和我聊天吧~");
+			textView.append("歡迎加我奧，輸入“擲筊”");
 		}
 
 		for (Event e : bundle.getEvents()) {
 			if (e.getType() == EventType.BLIP_SUBMITTED) {
-				Blip blip = wavelet.appendBlip();
-				TextView textView = blip.getDocument();
-				textView.append("我现在只会说你好,其他啥也不会~~");
+				Blip blip = e.getBlip();
+				String blipCreator = blip.getCreator();
+				String text = blip.getDocument().getText();
+				// Only process standard blips (i.e. not title blip)
+				if (blip.getBlipId().equals(wavelet.getRootBlipId()) == false) {
+					String response = fortunetellingProcess(blipCreator, text);
+					if (response != null) {
+						Blip responseBlip = wavelet.appendBlip();
+						TextView textView = responseBlip.getDocument();
+						textView.append(response);
+					}
+				}
 			}
 		}
 
+	}
+
+	private String fortunetellingProcess(String blipCreator, String body) {
+		if (cache == null)
+			return null;
+
+		long lastSessionTime = getSessionTime(blipCreator);
+
+		JSONObject jo = null;
+		if (lastSessionTime == 0) {
+			if (!body.equals(fortunetellingLogic.getAhit())) {
+				return null;
+			} else {
+				jo = fortunetellingLogic.reply(body, 0);
+			}
+		} else {
+			int step = getNextStep(blipCreator);
+			jo = fortunetellingLogic.reply(body, step);
+		}
+		if (jo != null) {
+			String reply = jo.getString("response");
+			setSessionTime(blipCreator);
+			setNextStep(blipCreator, jo.getInt("step"));
+			return reply;
+		}
+		return null;
+	}
+
+	private int getNextStep(String blipCreator) {
+		Object value = cache.get("_next_step");
+		if (value != null && value instanceof Integer)
+			return (Integer) value;
+		return 0;
+	}
+
+	private long getSessionTime(String blipCreator) {
+		Object value = cache.get("_session_time");
+		if (value != null && value instanceof Long)
+			return (Long) value;
+		return 0;
+	}
+
+	@SuppressWarnings("unchecked")
+	private void setNextStep(String blipCreator, int step) {
+		cache.put("_next_step", new Integer(step));
+	}
+
+	@SuppressWarnings("unchecked")
+	private void setSessionTime(String blipCreator) {
+		cache.put("_session_time", new Long(System.currentTimeMillis()));
 	}
 
 }
