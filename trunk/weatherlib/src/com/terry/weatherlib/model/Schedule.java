cc0e0deaf8ee1aedeab0df69b758357d948d1503
itcontent@gmail.com <itcontent@gmail.com@2f6c41ca-324e-0410-a569-9b2e432399e1>
2010-02-24 13:31:56 +0000

    weather add 2 models, ready to add schedule.

---
+++ trunk/weatherlib/src/com/terry/weatherlib/model/Schedule.java
@@ -0,0 +1,116 @@
+package com.terry.weatherlib.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.Transient;
+
+import org.datanucleus.jpa.annotations.Extension;
+
+/**
+ * @author xinghuo.yao E-mail: yaoxinghuo at 126 dot com
+ * @version create: 2010-2-3 下午03:35:27
+ */
+@Entity
+public class Schedule implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 2282192443072555696L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Extension(vendorName = "datanucleus", key = "gae.encoded-pk", value = "true")
+	private String id;
+
+	@Enumerated
+	private String account;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getAccount() {
+		return account;
+	}
+
+	public void setAccount(String account) {
+		this.account = account;
+	}
+
+	public Date getAdate() {
+		return adate;
+	}
+
+	public void setAdate(Date adate) {
+		this.adate = adate;
+	}
+
+	public Date getCdate() {
+		return cdate;
+	}
+
+	public void setCdate(Date cdate) {
+		this.cdate = cdate;
+	}
+
+	public Date getSdate() {
+		return sdate;
+	}
+
+	public void setSdate(Date sdate) {
+		this.sdate = sdate;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+
+	public String getNickname() {
+		return nickname;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	@Enumerated
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date adate;
+	@Enumerated
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date cdate;
+	@Enumerated
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date sdate;
+	@Enumerated
+	private String city;
+	@Enumerated
+	private String email;
+	@Transient
+	private String nickname;
+
+}
