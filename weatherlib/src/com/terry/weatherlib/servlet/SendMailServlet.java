44665c42cbad24e4bd2d9d8fb9d38f03cbfba411
yaoxinghuo <yaoxinghuo@2f6c41ca-324e-0410-a569-9b2e432399e1>
2010-02-25 14:37:37 +0000

    weatherlib ready to schedule forecast via robot.

---
+++ weatherlib/src/com/terry/weatherlib/servlet/SendMailServlet.java
@@ -0,0 +1,94 @@
+package com.terry.weatherlib.servlet;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Query;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.KeyFactory;
+import com.terry.weatherlib.Weather;
+import com.terry.weatherlib.WeatherCache;
+import com.terry.weatherlib.model.Account;
+import com.terry.weatherlib.model.Schedule;
+import com.terry.weatherlib.util.EMF;
+import com.terry.weatherlib.util.MailSender;
+import com.terry.weatherlib.util.StringUtil;
+
+/**
+ * @author xinghuo.yao E-mail: yaoxinghuo at 126 dot com
+ * @version create: 2010-2-3 下午04:35:50
+ */
+public class SendMailServlet extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -843840894946959108L;
+
+	private SimpleDateFormat sdf2 = new SimpleDateFormat("yyyy-MM-dd HH:mm",
+			Locale.CHINA);
+
+	@Override
+	public void init() throws ServletException {
+		sdf2.setTimeZone(TimeZone.getTimeZone("GMT+08:00"));
+	}
+
+	@Override
+	public void doGet(HttpServletRequest req, HttpServletResponse res)
+			throws IOException, ServletException {
+		doPost(req, res);
+	}
+
+	@Override
+	public void doPost(HttpServletRequest req, HttpServletResponse res)
+			throws IOException, ServletException {
+		String id = req.getParameter("id");
+		if (StringUtil.isEmptyOrWhitespace(id))
+			return;
+		Key key = KeyFactory.stringToKey(id);
+		if (key == null || !key.isComplete())
+			return;
+
+		EntityManager em = EMF.get().createEntityManager();
+
+		Schedule schedule = em.find(Schedule.class, key);
+		if (schedule == null)
+			return;
+		String a = schedule.getAccount();
+		Query query = em.createQuery("SELECT a FROM " + Account.class.getName()
+				+ " a where a.account=:account");
+		query.setParameter("account", a);
+		Account account = (Account) query.getSingleResult();
+		Weather weather = WeatherCache.queryWeather(schedule.getCity());
+		if (weather == null)
+			return;
+		try {
+			EntityTransaction tx = em.getTransaction();
+			tx.begin();
+			String content = weather.getContent();
+			String subject = content.split("  ")[0].trim();
+			MailSender.sendMail(schedule.getEmail(), account == null ? null
+					: account.getNickname(), a, subject, weather.getReport());
+			Date now = new Date();
+			Calendar c_sdate = Calendar.getInstance();
+			c_sdate.setTime(schedule.getSdate());
+			c_sdate.add(Calendar.DAY_OF_YEAR, 1);
+			schedule.setSdate(c_sdate.getTime());
+			schedule.setAdate(now);
+			em.persist(schedule);
+			tx.commit();
+		} catch (Exception e) {
+		}
+	}
+}
---
+++ weatherlib/src/com/terry/weatherlib/util/MailSender.java
@@ -0,0 +1,34 @@
+package com.terry.weatherlib.util;
+
+import java.util.Properties;
+
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeUtility;
+
+/**
+ * @author xinghuo.yao E-mail: yaoxinghuo at 126 dot com
+ * @version create: 2010-2-3 下午04:38:15
+ */
+public class MailSender {
+	public static void sendMail(String email, String sender, String replyTo,
+			String subject, String body) throws Exception {
+		Properties props = new Properties();
+		Session session = Session.getDefaultInstance(props, null);
+
+		javax.mail.Message msg = new MimeMessage(session);
+		if (StringUtil.isEmptyOrWhitespace(sender))
+			sender = replyTo.substring(0, replyTo.indexOf("@"));
+		msg.setFrom(new InternetAddress("service@appmail.org.ru", MimeUtility
+				.encodeText(sender, "UTF-8", "b")));
+		msg.setReplyTo(new InternetAddress[] { new InternetAddress(replyTo,
+				MimeUtility.encodeText(sender, "UTF-8", "b")) });
+		msg.addRecipient(javax.mail.Message.RecipientType.TO,
+				new InternetAddress(email));
+		msg.setSubject(MimeUtility.encodeText(subject, "UTF-8", "b"));
+		msg.setText(body);
+		Transport.send(msg);
+	}
+}
