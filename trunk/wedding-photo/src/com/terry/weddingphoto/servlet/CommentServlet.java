c22b27367d62e462358acf57c12e8174e62a1c3a
yaoxinghuo <yaoxinghuo@2f6c41ca-324e-0410-a569-9b2e432399e1>
2010-03-17 14:36:54 +0000

    wedding-photo add cache.

--- trunk/wedding-photo/src/com/terry/weddingphoto/servlet/CommentServlet.java
+++ trunk/wedding-photo/src/com/terry/weddingphoto/servlet/CommentServlet.java
@@ -14,6 +14,9 @@ import javax.servlet.http.HttpServletResponse;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import com.google.appengine.api.memcache.Expiration;
+import com.google.appengine.api.memcache.MemcacheService;
+import com.google.appengine.api.memcache.MemcacheServiceFactory;
 import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
 import com.terry.weddingphoto.data.impl.PhotoDaoImpl;
@@ -35,9 +38,13 @@ public class CommentServlet extends HttpServlet {
 	private SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm",
 			Locale.CHINA);
 	private UserService userService = UserServiceFactory.getUserService();
+	private MemcacheService cache = MemcacheServiceFactory.getMemcacheService();
+	private static final String IP_COUNT_CACHE = "ip-count-cache";
+	private static final int COMMENT_LIMIT = 5;
+	private static final int CACHE_SESSION_TIME = 3600;// 1个小时
 	private IPhotoDao photoDao = new PhotoDaoImpl();
 	private static final String ERROR = "对不起，程序出现错误，请稍候再试";
-	
+
 	@Override
 	public void init() throws ServletException {
 		sdf.setTimeZone(TimeZone.getTimeZone("GMT+08:00"));
@@ -89,15 +96,25 @@ public class CommentServlet extends HttpServlet {
 			return jo;
 		}
 
+		String ip = getIpAddr(req);
+		if (getIpCommentCount(pid, ip) >= COMMENT_LIMIT) {
+			try {
+				jo.put("message", "您的评论频率太高");
+			} catch (JSONException e) {
+			}
+			return jo;
+		}
+
 		Comment comment = new Comment();
 		comment.setCdate(new Date());
-		comment.setIp(getIpAddr(req));
+		comment.setIp(ip);
 		comment.setEmail(email);
 		comment.setName(nickname);
 		comment.setPid(pid);
 		comment.setContent(content);
 		int count = photoDao.saveComment(comment);
 		if (count != -1) {
+			updateIpCommentCount(pid, ip);
 			try {
 				jo.put("result", true);
 				jo.put("message", "已成功发布评论");
@@ -129,8 +146,8 @@ public class CommentServlet extends HttpServlet {
 			}
 			return jo;
 		}
-		int count =photoDao.deleteCommentById(cid);
-		if (count!=-1) {
+		int count = photoDao.deleteCommentById(cid);
+		if (count != -1) {
 			try {
 				jo.put("result", true);
 				jo.put("message", "已成功删除评论");
@@ -165,4 +182,21 @@ public class CommentServlet extends HttpServlet {
 		return ip;
 	}
 
+	private int getIpCommentCount(String pid, String ip) {
+		String key = IP_COUNT_CACHE + "-" + pid + "-" + ip;
+		Object o = cache.get(key);
+		if (o != null && o instanceof Integer) {
+			return (Integer) o;
+		}
+		return 0;
+	}
+
+	private void updateIpCommentCount(String pid, String ip) {
+		String key = IP_COUNT_CACHE + "-" + pid + "-" + ip;
+		Object o = cache.get(key);
+		if (o != null && o instanceof Integer) {
+			cache.increment(key, 1);
+		} else
+			cache.put(key, 1, Expiration.byDeltaSeconds(CACHE_SESSION_TIME));
+	}
 }
---
+++ trunk/wedding-photo/src/com/terry/weddingphoto/util/PhotoCache.java
@@ -0,0 +1,83 @@
+package com.terry.weddingphoto.util;
+
+import java.util.Collections;
+
+import javax.cache.Cache;
+import javax.cache.CacheException;
+import javax.cache.CacheManager;
+
+import com.google.appengine.api.images.Image;
+import com.google.appengine.api.images.ImagesService;
+import com.google.appengine.api.images.ImagesServiceFactory;
+import com.google.appengine.api.images.Transform;
+import com.terry.weddingphoto.data.impl.PhotoDaoImpl;
+import com.terry.weddingphoto.data.intf.IPhotoDao;
+import com.terry.weddingphoto.model.Photo;
+
+/**
+ * @author Terry E-mail: yaoxinghuo at 126 dot com
+ * @version create: Mar 17, 2010 9:28:41 PM
+ */
+public class PhotoCache {
+	private static Cache cache;
+
+	private static final String PHOTO_CACHE = "photo-cache";
+
+	private static ImagesService imagesService = ImagesServiceFactory
+			.getImagesService();
+	private static IPhotoDao photoDao = new PhotoDaoImpl();
+
+	static {
+		try {
+			cache = CacheManager.getInstance().getCacheFactory().createCache(
+					Collections.emptyMap());
+		} catch (CacheException e) {
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	public static byte[] getPhotoData(String pid, String width, String height) {
+		if (width == null)
+			width = "";
+		if (height == null)
+			height = "";
+		String key = PHOTO_CACHE + "-" + pid + "-w" + width + "-h" + height;
+		Object o = cache.get(key);
+		if (o != null)
+			return (byte[]) o;
+		byte[] data = getPhotoDataFromDB(pid, width, height);
+		if (data != null) {
+			if (data.length < 1024 * 1024)
+				cache.put(key, data);
+		}
+		return data;
+	}
+
+	private static byte[] getPhotoDataFromDB(String pid, String width,
+			String height) {
+		Photo p = photoDao.getPhotoById(pid);
+		if (p != null) {
+			byte[] data = p.getData().getBytes();
+			if (!StringUtil.isEmptyOrWhitespace(width)
+					&& !StringUtil.isEmptyOrWhitespace(height)) {
+				int w = Integer.parseInt(width);
+				int h = Integer.parseInt(height);
+				Image oldImage = ImagesServiceFactory.makeImage(data);
+				if (w <= 0)
+					w = oldImage.getWidth();
+				if (h <= 0)
+					h = oldImage.getHeight();
+				if (oldImage.getWidth() > w || oldImage.getHeight() > h) {
+					Transform resize = ImagesServiceFactory.makeResize(w, h);
+					Image newImage = imagesService.applyTransform(resize,
+							oldImage);
+					return newImage.getImageData();
+				} else
+					return data;
+			} else
+				return data;
+		}
+		return null;
+	}
+
+}
