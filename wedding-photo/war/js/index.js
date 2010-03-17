f4e4c21ff2c67e1857c831609cc4a897f00f1d52
yaoxinghuo <yaoxinghuo@2f6c41ca-324e-0410-a569-9b2e432399e1>
2010-03-17 13:29:19 +0000

    wedding-photo small change.

--- wedding-photo/war/js/index.js
+++ wedding-photo/war/js/index.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
-	var wh = getPageSize();
 	$('#photos').galleryView( {
-		panel_width : wh[0] - 76,
+		panel_width : document.documentElement.clientWidth - 76,
 		panel_height : 10,
 		frame_width : 100,
 		frame_height : 80,
@@ -23,7 +22,7 @@ $(document).ready(function() {
 		drag : true,
 		iframeWH : {
 			width : 700,
-			height : wh[1]-200<=0?400:wh[1]-200
+			height : 580
 		},
 		title : '评论'
 	});
@@ -34,54 +33,3 @@ $(document).ready(function() {
 		});
 	});
 });
-
-function getPageSize() {
-	var xScroll, yScroll;
-	if (window.innerHeight && window.scrollMaxY) {
-		xScroll = window.innerWidth + window.scrollMaxX;
-		yScroll = window.innerHeight + window.scrollMaxY;
-	} else if (document.body.scrollHeight > document.body.offsetHeight) { // all
-		// but
-		// Explorer
-		// Mac
-		xScroll = document.body.scrollWidth;
-		yScroll = document.body.scrollHeight;
-	} else { // Explorer Mac...would also work in Explorer 6 Strict, Mozilla
-		// and Safari
-		xScroll = document.body.offsetWidth;
-		yScroll = document.body.offsetHeight;
-	}
-	var windowWidth, windowHeight;
-	if (self.innerHeight) { // all except Explorer
-		if (document.documentElement.clientWidth) {
-			windowWidth = document.documentElement.clientWidth;
-		} else {
-			windowWidth = self.innerWidth;
-		}
-		windowHeight = self.innerHeight;
-	} else if (document.documentElement
-			&& document.documentElement.clientHeight) { // Explorer 6 Strict
-		// Mode
-		windowWidth = document.documentElement.clientWidth;
-		windowHeight = document.documentElement.clientHeight;
-	} else if (document.body) { // other Explorers
-		windowWidth = document.body.clientWidth;
-		windowHeight = document.body.clientHeight;
-	}
-	// for small pages with total height less then height of the viewport
-	if (yScroll < windowHeight) {
-		pageHeight = windowHeight;
-	} else {
-		pageHeight = yScroll;
-	}
-	// for small pages with total width less then width of the viewport
-	if (xScroll < windowWidth) {
-		pageWidth = xScroll;
-	} else {
-		pageWidth = windowWidth;
-	}
-	pageWidth = Math.max(pageWidth, windowWidth);
-	pageHeight = Math.max(pageHeight, windowHeight);
-	arrayPageSize = new Array(pageWidth, pageHeight);
-	return arrayPageSize;
-};
