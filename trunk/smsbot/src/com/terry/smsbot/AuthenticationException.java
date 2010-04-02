0766fe0df7ad8a94c900a2eb1fd2e29ea740dbd8
yaoxinghuo <yaoxinghuo@2f6c41ca-324e-0410-a569-9b2e432399e1>
2010-04-02 15:18:44 +0000

    smsbot change AuthenticationExeption to AuthenticationException.

--- trunk/smsbot/src/com/terry/smsbot/AuthenticationException.java
+++ trunk/smsbot/src/com/terry/smsbot/AuthenticationException.java
@@ -4,14 +4,14 @@ package com.terry.smsbot;
  * @author xinghuo.yao E-mail: yaoxinghuo at 126 dot com
  * @version create: 2010-2-10 上午10:45:03
  */
-public class AuthenticationExeption extends Exception {
+public class AuthenticationException extends Exception {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 3711466252389933243L;
 
-	public AuthenticationExeption(String message) {
+	public AuthenticationException(String message) {
 		super(message);
 	}
 
