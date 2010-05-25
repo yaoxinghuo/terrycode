35268fe72987ae1dc0dbb2d5c662bf4954daadb6
itcontent@gmail.com <itcontent@gmail.com@2f6c41ca-324e-0410-a569-9b2e432399e1>
2010-05-25 06:44:18 +0000

    weatherlib2 fetch weather from wap.weather.com.cn/wap

---
+++ trunk/weatherlib2/src/com/terry/weatherlib/WeatherCache.java
@@ -0,0 +1,122 @@
+package com.terry.weatherlib;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.cache.Cache;
+import javax.cache.CacheException;
+import javax.cache.CacheManager;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import com.google.appengine.api.memcache.Expiration;
+import com.google.appengine.api.memcache.MemcacheService;
+import com.google.appengine.api.memcache.MemcacheServiceFactory;
+import com.google.appengine.api.memcache.stdimpl.GCacheFactory;
+import com.terry.weatherlib.util.Constants;
+
+/**
+ * @author xinghuo.yao E-mail: yaoxinghuo at 126 dot com
+ * @version create: 2010-2-24 下午12:20:15
+ */
+public class WeatherCache {
+
+	private static Cache cache;
+
+	private static Log log = LogFactory.getLog(WeatherCache.class);
+
+	private static MemcacheService cacheService = MemcacheServiceFactory
+			.getMemcacheService();
+
+	static {
+		Map<Integer, Integer> props = new HashMap<Integer, Integer>();
+		props.put(GCacheFactory.EXPIRATION_DELTA,
+				Constants.DEFAULT_WEATHER_CACHE_TIME);
+		try {
+			cache = CacheManager.getInstance().getCacheFactory().createCache(
+					props);
+		} catch (CacheException e) {
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	public static Weather queryWeather(String loc, String suffix) {
+		String key = Constants.DEFAULT_WEATHER_CACHE_NAME + "-"
+				+ (suffix == null ? loc : suffix);
+		if (cache != null) {
+			Object o = cache.get(key);
+			if (o != null && o instanceof Weather) {
+				return (Weather) o;
+			}
+		}
+
+		Weather w = WeatherFetcher.fetchWeather(loc);
+		if (w != null && cache != null) {
+			cacheService.put(key, w, getExpiration());
+
+			// Add weather cache name to cache.
+			ArrayList<String> names = null;
+			Object o = cache.get(Constants.DEFAULT_CACHE_CACHE_NAME);
+			if (o != null && o instanceof ArrayList<?>)
+				names = (ArrayList<String>) o;
+			else
+				names = new ArrayList<String>();
+			names.add(key);
+			cache.put(Constants.DEFAULT_CACHE_CACHE_NAME, names);
+		}
+		return w;
+	}
+
+	public static Weather queryWeather(String loc) {
+		return queryWeather(loc, null);
+	}
+
+	/*
+	 * http://www.google.com/m的天气预报每天8:35 17:35更新一次，还有凌晨也要手动更新一次
+	 * 
+	 * 所以要得到Cache的保存时间
+	 */
+	public static Expiration getExpiration() {
+		long now = System.currentTimeMillis();
+		Calendar c = Calendar.getInstance();
+		c.setTimeInMillis(now);
+		c.set(Calendar.HOUR_OF_DAY, 0);// 8:32
+		c.set(Calendar.MINUTE, 35);
+		boolean add = false;
+		if (c.getTimeInMillis() <= now) {
+			c.add(Calendar.DAY_OF_YEAR, 1);
+			add = true;
+		}
+		long x = c.getTimeInMillis();
+
+		if (add) {
+			c.add(Calendar.DAY_OF_YEAR, -1);
+			add = false;
+		}
+		c.set(Calendar.HOUR_OF_DAY, 9);// 17:32
+		c.set(Calendar.MINUTE, 35);
+		if (c.getTimeInMillis() <= now) {
+			c.add(Calendar.DAY_OF_YEAR, 1);
+			add = true;
+		}
+		long y = c.getTimeInMillis();
+
+		if (add) {
+			c.add(Calendar.DAY_OF_YEAR, -1);
+			add = false;
+		}
+		c.set(Calendar.HOUR_OF_DAY, 16);// 00:00
+		c.set(Calendar.MINUTE, 10);
+		if (c.getTimeInMillis() <= now)
+			c.add(Calendar.DAY_OF_YEAR, 1);
+		long z = c.getTimeInMillis();
+
+		Date minDate = new Date(Math.min(x, Math.min(y, z)));
+		log.debug("minDate:" + minDate.toString());
+		return Expiration.onDate(minDate);
+	}
+}
---
+++ trunk/weatherlib2/src/com/terry/weatherlib/WeatherFetcher.java
@@ -0,0 +1,140 @@
+package com.terry.weatherlib;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.htmlparser.Node;
+import org.htmlparser.NodeFilter;
+import org.htmlparser.Parser;
+import org.htmlparser.filters.NodeClassFilter;
+import org.htmlparser.tags.Div;
+import org.htmlparser.tags.ImageTag;
+import org.htmlparser.util.NodeList;
+import org.htmlparser.util.ParserException;
+
+/**
+ * @author xinghuo.yao E-mail: yaoxinghuo at 126 dot com
+ * @version create: 2010-2-23 下午05:20:54
+ */
+public class WeatherFetcher {
+
+	private static Log log = LogFactory.getLog(WeatherFetcher.class);
+
+	public static Weather fetchWeather(String loc) {
+		String unicodeLoc = null;
+		try {
+			unicodeLoc = URLEncoder.encode(loc, "UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			return null;
+		}
+		String data = fetchData("http://www.google.com.hk/m?q=" + unicodeLoc
+				+ "&site=weather&gl=cn&hl=zh-CN");
+		log.debug("fetch data:" + loc + data);
+		if (data == null)
+			return null;
+		return parserWeather(data, loc);
+	}
+
+	private static String fetchData(String url) {
+		try {
+			HttpURLConnection con = (HttpURLConnection) new URL(url)
+					.openConnection();
+			con.setConnectTimeout(10000);
+			con.setReadTimeout(10000);
+			con
+					.setRequestProperty(
+							"User-Agent",
+							"Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US) AppleWebKit/533.1 (KHTML, like Gecko) Chrome/5.0.322.2 Safari/533.1,gzip(gfe),gzip(gfe)");
+			con.setRequestMethod("GET");
+
+			int code = con.getResponseCode();
+			if (code >= 200 && code < 300) {
+				BufferedReader reader = new BufferedReader(
+						new InputStreamReader(con.getInputStream(), "UTF-8")); // 读取结果
+				StringBuffer sb = new StringBuffer("");
+				String line;
+				while ((line = reader.readLine()) != null) {
+					sb.append(line).append("\r\n");
+				}
+				reader.close();
+				con.disconnect();
+				return sb.toString();
+			}
+			con.disconnect();
+			return null;
+		} catch (Exception e) {
+			return null;
+		}
+	}
+
+	public static Weather parserWeather(String html, String loc) {
+		Parser parser = Parser.createParser(html.replace("<br/>", "\r\n")
+				.replace("&nbsp;", " "), "utf8");
+
+		NodeFilter contentFilter = new NodeClassFilter(Div.class);
+		NodeList nodelist = null;
+		try {
+			nodelist = parser.extractAllNodesThatMatch(contentFilter);
+		} catch (ParserException e) {
+			return null;
+		}
+
+		Node[] nodes = nodelist.toNodeArray();
+
+		Weather weather = new Weather();
+
+		for (int i = 0; i < nodes.length; i++) {
+			Node node = nodes[i];
+			if (node instanceof Div) {
+				Div div = (Div) node;
+				String className = div.getAttribute("class");
+				if (className == null
+						&& div.getFirstChild() instanceof ImageTag) {
+					weather.setContent(div.toPlainTextString().trim());
+					log.debug("content:" + weather.getContent());
+				}
+				if (className != null) {
+					if (className.equals("b")) {
+						String city = div.toPlainTextString().trim();
+						if (city.contains("天气") && !city.contains(" ")
+								&& (city.length() - loc.length()) < 5) {
+							// http://www.google.com.hk/m/search?site=weather&gl=cn&hl=zh-CN&q=长沙市
+							// 输入长沙，却出来的是长沙县，怪怪的，所以这里if一下
+							if (loc.contains("长沙") && city.contains(loc))
+								weather.setCity(loc);
+							else
+								weather.setCity(city.replace("天气", ""));
+							log.debug("city:" + weather.getCity());
+						}
+					}
+				}
+
+			}
+		}
+		if (weather.getCity() == null)
+			weather.setCity(loc);
+		if (weather.checkComplete()) {
+			weather.setUdate(new Date());
+			return weather;
+		}
+		return null;
+	}
+
+	public static void main(String[] args) throws Exception {
+		// Weather w = WeatherFetcher.fetchWeather("上海");
+		// if (w != null)
+		// System.out.println(w.getReport());
+
+		SimpleDateFormat sdf2 = new SimpleDateFormat("M月d日H:mm", Locale.CHINA);
+		System.out.println(sdf2.format(new Date()));
+	}
+}
