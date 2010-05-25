fbbc6c0edd7422d3dc24a1db2d12d87c7c17e2bb
itcontent@gmail.com <itcontent@gmail.com@2f6c41ca-324e-0410-a569-9b2e432399e1>
2010-05-25 06:44:18 +0000

    weatherlib2 fetch weather from wap.weather.com.cn/wap

---
+++ weatherlib2/src/com/terry/weatherlib/servlet/ClearWeatherCacheServlet.java
@@ -0,0 +1,68 @@
+package com.terry.weatherlib.servlet;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+
+import javax.cache.Cache;
+import javax.cache.CacheException;
+import javax.cache.CacheManager;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import com.terry.weatherlib.util.Constants;
+
+/**
+ * @author Terry E-mail: yaoxinghuo at 126 dot com
+ * @version create: Feb 27, 2010 8:50:27 AM
+ */
+public class ClearWeatherCacheServlet extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1883179443719794954L;
+
+	private static Log log = LogFactory.getLog(ClearWeatherCacheServlet.class);
+
+	private Cache cache;
+
+	@Override
+	public void init() throws ServletException {
+		try {
+			cache = CacheManager.getInstance().getCacheFactory().createCache(
+					Collections.emptyMap());
+		} catch (CacheException e) {
+		}
+	}
+
+	@Override
+	public void doGet(HttpServletRequest req, HttpServletResponse res)
+			throws IOException, ServletException {
+		doPost(req, res);
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public void doPost(HttpServletRequest req, HttpServletResponse res)
+			throws IOException, ServletException {
+		if (cache == null)
+			return;
+		Object o = cache.get(Constants.DEFAULT_CACHE_CACHE_NAME);
+		if (o != null && o instanceof ArrayList<?>) {
+			ArrayList<String> names = (ArrayList<String>) o;
+			log.debug("remove " + names.size() + " weather cache names.");
+			for (String name : names) {
+				cache.remove(name);
+			}
+			cache.put(Constants.DEFAULT_CACHE_CACHE_NAME,
+					new ArrayList<String>());
+		} else
+			cache.clear();
+	}
+}
