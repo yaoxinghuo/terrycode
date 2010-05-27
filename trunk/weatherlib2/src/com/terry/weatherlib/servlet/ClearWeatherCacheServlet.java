bb9b5354230208402d0932ae38e429bc56749d1f
itcontent@gmail.com <itcontent@gmail.com@2f6c41ca-324e-0410-a569-9b2e432399e1>
2010-05-27 03:25:35 +0000

    weatherlib2 change cache clear policy.

--- trunk/weatherlib2/src/com/terry/weatherlib/servlet/ClearWeatherCacheServlet.java
+++ trunk/weatherlib2/src/com/terry/weatherlib/servlet/ClearWeatherCacheServlet.java
@@ -62,9 +62,10 @@ public class ClearWeatherCacheServlet extends HttpServlet {
 	public void doPost(HttpServletRequest req, HttpServletResponse res)
 			throws IOException, ServletException {
 
-		if (!checkShouldClearCache())
+		if (!checkShouldClearCache()){
+			log.debug("there is no need to clear weather cache.");
 			return;
-
+		}
 		Object o = cache.get(Constants.DEFAULT_CACHE_CACHE_NAME);
 		if (o != null && o instanceof ArrayList<?>) {
 			ArrayList<String> names = (ArrayList<String>) o;
@@ -82,30 +83,29 @@ public class ClearWeatherCacheServlet extends HttpServlet {
 		if (cache == null)
 			return false;
 		long nowTime = System.currentTimeMillis();
-		Weather cacheTextWeather = WeatherCache.queryWeather(testCity);
-		if (cacheTextWeather == null
-				|| cacheTextWeather.getUdate().getTime() >= nowTime)
+		Weather cacheTestWeather = WeatherCache.queryWeather(testCity);
+		if (cacheTestWeather == null
+				|| cacheTestWeather.getUdate().getTime() >= nowTime)
 			return false;// 如果是刚刚从网上取的，就不用检查了
 
-		String hourS = getHour(cacheTextWeather.getDesc());
+		String hourS = getHour(cacheTestWeather.getDesc());
 		if (hourS == null)
 			return false;
 		int cacheHour = Integer.parseInt(hourS);
 		int nowHour = Calendar.getInstance(TimeZone.getTimeZone("GMT+08:00"),
 				Locale.CHINA).get(Calendar.HOUR_OF_DAY);
-		if (nowHour >= 0 && nowHour < 8)
-			cacheHour = 0;
-		if (cacheHour == 0) {
-			if (cacheHour < 8)
+
+		if (nowHour >= 0 && nowHour < 8) {
+			if (cacheHour == 18)
 				return false;
-		} else if (cacheHour == 8) {
-			if (nowHour < 11)
+		} else if (nowHour >= 8 && nowHour < 12) {
+			if (cacheHour == 8)
 				return false;
-		} else if (cacheHour == 11) {
-			if (nowHour < 18)
+		} else if (nowHour >= 12 && nowHour < 18) {
+			if (cacheHour == 11)
 				return false;
-		} else if (cacheHour == 18) {
-			if (nowHour <= 23)
+		} else {
+			if (cacheHour == 18)
 				return false;
 		}
 
@@ -115,7 +115,7 @@ public class ClearWeatherCacheServlet extends HttpServlet {
 		 * 如果从cache取出来的和直接从网上取到的desc（类似2010-05-01 11时发布）不一样，就要清空缓存
 		 */
 		if (nowTestWeather == null || nowTestWeather.getDesc() == null
-				|| nowTestWeather.getDesc().equals(cacheTextWeather.getDesc()))
+				|| nowTestWeather.getDesc().equals(cacheTestWeather.getDesc()))
 			return false;
 		else
 			return true;
