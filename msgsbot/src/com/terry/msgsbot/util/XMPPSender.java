9d5bac62af16f80c59630079ba71fb9c4422b379
itcontent@gmail.com <itcontent@gmail.com@2f6c41ca-324e-0410-a569-9b2e432399e1>
2011-02-12 03:32:37 +0000

    msgsbot update status check to xmpp robot.

---
+++ msgsbot/src/com/terry/msgsbot/util/XMPPSender.java
@@ -0,0 +1,50 @@
+package com.terry.msgsbot.util;
+
+import com.google.appengine.api.xmpp.JID;
+import com.google.appengine.api.xmpp.MessageBuilder;
+import com.google.appengine.api.xmpp.PresenceShow;
+import com.google.appengine.api.xmpp.PresenceType;
+import com.google.appengine.api.xmpp.SendResponse;
+import com.google.appengine.api.xmpp.XMPPService;
+import com.google.appengine.api.xmpp.XMPPServiceFactory;
+
+/**
+ * @author Terry E-mail: yaoxinghuo at 126 dot com
+ * @version create: Feb 4, 2010 10:06:10 PM
+ */
+public class XMPPSender {
+	private static XMPPService xmpp = XMPPServiceFactory.getXMPPService();
+
+	public static boolean sendXMPP(String account, String content) {
+		JID jid = new JID(account);
+		return sendXMPP(jid, content);
+	}
+
+	public static boolean sendXMPP(JID jid, String content) {
+		try {
+			if (!xmpp.getPresence(jid).isAvailable()
+					&& jid.getId().indexOf("@appspot.com") == -1)
+				return false;
+			SendResponse status = xmpp.sendMessage(new MessageBuilder()
+					.withRecipientJids(jid).withBody(content).build());
+			return (status.getStatusMap().get(jid) == SendResponse.Status.SUCCESS);
+		} catch (Exception e) {
+			return false;
+		}
+	}
+
+	public static void sendStatus(String account, String status) {
+		JID jid = new JID(account);
+		sendStatus(jid, status);
+	}
+
+	public static void sendStatus(JID jid, String status) {
+		try {
+			if (jid.getId().indexOf("@appspot.com") != -1)
+				return;
+			xmpp.sendPresence(jid, PresenceType.AVAILABLE, PresenceShow.CHAT,
+					status);
+		} catch (Exception e) {
+		}
+	}
+}
