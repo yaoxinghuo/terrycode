4ebe0dc0f608a3b4f9aaa67ee4b3ab0ffa9bc55f
itcontent@gmail.com <itcontent@gmail.com@2f6c41ca-324e-0410-a569-9b2e432399e1>
2011-08-02 08:04:05 +0000

    botmail update botmail.

--- trunk/botmail/src/com/terry/botmail/util/MailSender.java
+++ trunk/botmail/src/com/terry/botmail/util/MailSender.java
@@ -8,6 +8,8 @@ import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 import javax.mail.internet.MimeUtility;
 
+import com.google.appengine.api.utils.SystemProperty;
+
 /**
  * @author xinghuo.yao E-mail: yaoxinghuo at 126 dot com
  * @version create: 2010-2-3 下午04:38:15
@@ -27,7 +29,9 @@ public class MailSender {
 		 * 
 		 * 不能随意设置发件人
 		 */
-		msg.setFrom(new InternetAddress("service@appmail.org.ru", MimeUtility
+		String from = "service@" + SystemProperty.applicationId.get()
+		+ ".appspotmail.com";
+		msg.setFrom(new InternetAddress(from, MimeUtility
 				.encodeText(sender, "UTF-8", "b")));
 		msg.addRecipient(javax.mail.Message.RecipientType.TO,
 				new InternetAddress(email));
