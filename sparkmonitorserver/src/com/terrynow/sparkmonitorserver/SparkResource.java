c18d7fe0ef55796009c9256f60bfba15fdae8028
itcontent@gmail.com <itcontent@gmail.com@2f6c41ca-324e-0410-a569-9b2e432399e1>
2013-03-14 03:27:14 +0000

    搞了一个spark post url发送监控信息的小程序

---
+++ sparkmonitorserver/src/com/terrynow/sparkmonitorserver/SparkResource.java
@@ -0,0 +1,96 @@
+package com.terrynow.sparkmonitorserver;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.jivesoftware.smack.Chat;
+import org.jivesoftware.smack.MessageListener;
+import org.jivesoftware.smack.XMPPConnection;
+import org.jivesoftware.smack.packet.Message;
+import org.restlet.data.Form;
+import org.restlet.data.Status;
+import org.restlet.representation.Representation;
+import org.restlet.representation.StringRepresentation;
+import org.restlet.resource.ResourceException;
+import org.restlet.resource.ServerResource;
+
+/**
+ * @author Terry E-mail: yaoxinghuo at 126 dot com
+ * @date 2012-3-27 下午2:09:59
+ */
+public class SparkResource extends ServerResource {
+
+	private static Log log = LogFactory.getLog(ServerResource.class);
+
+	@Override
+	protected Representation post(Representation entity)
+			throws ResourceException {
+		Form form = new Form(entity);
+		String username = form.getFirstValue("username");
+		String password = form.getFirstValue("password");
+		String host = form.getFirstValue("host");
+		String to = form.getFirstValue("to");
+		String message = form.getFirstValue("message");
+
+		if (StringUtil.isEmptyOrWhitespace(username)
+				|| StringUtil.isEmptyOrWhitespace(password)
+				|| StringUtil.isEmptyOrWhitespace(host)
+				|| StringUtil.isEmptyOrWhitespace(to)
+				|| StringUtil.isEmptyOrWhitespace(message)) {
+			getResponse()
+					.setStatus(Status.CLIENT_ERROR_BAD_REQUEST,
+							"Parameter: username, password,host,to,message can not be empty.");
+			return defaultSR();
+		}
+		String error = sendMsg(host, username, password, to, message);
+		if (error == null) {
+			getResponse().setStatus(Status.SUCCESS_OK);
+			log.info("send message to: " + to + " ok");
+		} else {
+			log.info("send message to: " + to + " error: " + error);
+		}
+
+		return defaultSR();
+	}
+
+	private StringRepresentation defaultSR() {
+		return new StringRepresentation("OK");
+	}
+
+	@Override
+	protected Representation get() throws ResourceException {
+		return super.get();
+	}
+
+	private String sendMsg(String host, String username, String password,
+			String to, String message) {
+		XMPPConnection conn = new XMPPConnection(host);
+		try {
+			conn.connect();
+			conn.login(username, password); // 发送端的用户名密码
+			Chat mychat = conn.getChatManager().createChat(to, // 接收端的JID，这个JID是要加域的，不然会报错
+					new MessageListener() {
+						@Override
+						public void processMessage(Chat chat, Message message) {
+							// String messageBody = message.getBody();
+							// System.out.println("收到信息：" + messageBody + " "
+							// + message.getFrom());
+						}
+					});
+			// System.out.println("我的好友列表：=======================");
+			// Collection<RosterEntry> rosters = conn.getRoster().getEntries();
+			// for (RosterEntry rosterEntry : rosters) {
+			// System.out.print("name: " + rosterEntry.getName() + ",jid: "
+			// + rosterEntry.getUser()); // 此处可获取用户JID
+			// System.out.println("");
+			// }
+			// System.out.println("我的好友列表：=======================");
+
+			mychat.sendMessage(message); // 发送信息
+			return null;
+		} catch (Exception e) {
+			return e.getMessage() == null ? "ERROR" : e.getMessage();
+		} finally {
+			conn.disconnect(); // 断开连接
+		}
+	}
+}
