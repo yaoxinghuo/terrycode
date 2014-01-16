974a3e9935758a2184e4c879b3ed0bf29fc3c3bd
yaoxinghuo@gmail.com <yaoxinghuo@gmail.com@2f6c41ca-324e-0410-a569-9b2e432399e1>
2014-01-16 06:19:19 +0000

    now support google gtalk to hangout.

--- trunk/sparkmonitorserver/src/com/terrynow/sparkmonitorserver/ClientTest.java
+++ trunk/sparkmonitorserver/src/com/terrynow/sparkmonitorserver/ClientTest.java
@@ -11,17 +11,18 @@ import java.net.URLEncoder;
  */
 public class ClientTest {
 	/**
-	 * 添加了一个Outlook收到邮件后，发送spark提醒的例子，Outlook中建立规则，自定义脚本，然后新建宏，参考Outlook_VBS_Bas1.bas
+	 * 添加了一个Outlook收到邮件后，发送spark提醒的例子，Outlook中建立规则，自定义脚本，然后新建宏，
+	 * 参考Outlook_VBS_Bas1.bas
 	 * 
 	 * @param args
 	 * @author Terry
 	 * @date 2013-3-14
 	 */
-	//http://localhost:8182/spark?username=monitor&password=12345678&host=im.e104.com.cn&to=xinghuo.yao@spark.e104.com.cn&message=test
+	// http://localhost:8182/spark?username=monitor&password=12345678&host=im.e104.com.cn&to=xinghuo.yao@spark.e104.com.cn&message=test
 	public static void main(String[] args) {
 		fetchToSendMsg("http://localhost:8182/spark", "im.e104.com.cn",
 				"monitor", "12345678", "xinghuo.yao@spark.e104.com.cn",
-				"今天的天气好极了");	
+				"今天的天气好极了");
 	}
 
 	public static boolean fetchToSendMsg(String url, String host,
--- trunk/sparkmonitorserver/src/com/terrynow/sparkmonitorserver/SparkResource.java
+++ trunk/sparkmonitorserver/src/com/terrynow/sparkmonitorserver/SparkResource.java
@@ -1,10 +1,15 @@
 package com.terrynow.sparkmonitorserver;
 
+import java.io.FileInputStream;
+import java.util.Properties;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.jivesoftware.smack.Chat;
+import org.jivesoftware.smack.ConnectionConfiguration;
 import org.jivesoftware.smack.MessageListener;
 import org.jivesoftware.smack.XMPPConnection;
+import org.jivesoftware.smack.XMPPException;
 import org.jivesoftware.smack.packet.Message;
 import org.restlet.data.Form;
 import org.restlet.data.Status;
@@ -45,10 +50,12 @@ public class SparkResource extends ServerResource {
 				|| StringUtil.isEmptyOrWhitespace(message)) {
 			getResponse()
 					.setStatus(Status.CLIENT_ERROR_BAD_REQUEST,
-							"Parameter: username, password,host,to,message can not be empty.");
+							"Parameter: username, password, host, to, message can not be empty.");
 			return defaultSR();
 		}
-		String error = sendMsg(host, username, password, to, message);
+		String error = "google".equals(host) ? sendMsgGoogle(username,
+				password, to, message) : sendMsg(host, username, password, to,
+				message);
 		if (error == null) {
 			getResponse().setStatus(Status.SUCCESS_OK);
 			log.info("send message to: " + to + " ok");
@@ -96,5 +103,43 @@ public class SparkResource extends ServerResource {
 		} finally {
 			conn.disconnect(); // 断开连接
 		}
+	}
+
+	private String sendMsgGoogle(String username, String password, String to,
+			String message) {
+		String talkGoogle = "173.194.72.125";
+		int port = 5222;
+		String gmailServer = "173.194.72.17";
+		try {
+			ClassLoader classLoader = SparkResource.class.getClassLoader();
+			Properties properties = new Properties();
+			properties.load(new FileInputStream(classLoader.getResource(
+					"google.properties").getFile()));
+			talkGoogle = properties.getProperty("google.host", talkGoogle);
+			port = Integer.parseInt(properties.getProperty("google.port",
+					String.valueOf(port)));
+			gmailServer = properties.getProperty("google.serviceName",
+					gmailServer);
+		} catch (Exception e) {
+		}
+
+		ConnectionConfiguration connConfig = new ConnectionConfiguration(
+				talkGoogle, port, gmailServer);
+		XMPPConnection connection = new XMPPConnection(connConfig);
+		try {
+			connection.connect();
+			connection.login(username, password);
+
+			Message msg = new Message(to, Message.Type.chat);
+			msg.setBody(message);
+			connection.sendPacket(msg);
+
+			return null;
+		} catch (XMPPException e) {
+			return e.getMessage() == null ? "ERROR" : e.getMessage();
+		} finally {
+			connection.disconnect();
+		}
+
 	}
 }
